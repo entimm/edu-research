7c9080e0f16d3c905da01d8b6073e0a9877bceb4
enjoy.liu <enjoy.liu@huolala.cn>
2021-10-23 19:44:36 +0800

    UPDATE

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,17 +11,17 @@ class UserController extends Controller
 {
     public function loginView(Request $request, $code = 0)
     {
-        $skey = session('skey');
-        if (!$skey) {
-            $skey = md5($request->userAgent().'||'.$request->ip());
-            session(['skey' => $skey]);
-            Redis::hmset('skey_list', $skey, $request->userAgent().'||'.$request->ip());
+        $clientKey = session('clientKey');
+        if (!$clientKey) {
+            $clientKey = md5($request->userAgent().'||'.$request->ip());
+            session(['clientKey' => $clientKey]);
+            Redis::hmset('clientKeyList', $clientKey, $request->userAgent().'||'.$request->ip());
         }
 
         $historyLogin = array_map(function ($item) {
             $item = json_decode($item, true);
             return $item;
-        }, Redis::smembers($skey));
+        }, Redis::smembers($clientKey));
         if (88 == $code) {
             session(['admin' => 1]);
         }
@@ -40,20 +40,12 @@ class UserController extends Controller
             'age' => $request->age,
             'sex' => $request->sex,
             'student_no' => $request->student_no,
-            'user_agent' => $request->userAgent(),
-            'ip' => $request->ip(),
         ];
-        Redis::srem($key, json_encode([
-            'school' => $request->school,
-            'class' => $request->class,
-            'name' => $request->name,
-            'grade' => $request->grade,
-            'age' => $request->age,
-            'student_no' => $request->student_no,
-        ]));
         Redis::sadd($key, json_encode($data));
         session($data);
         DB::table('login_log')->insert(array_merge($data, [
+            'user_agent' => $request->userAgent(),
+            'ip' => $request->ip(),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]));
@@ -78,19 +70,19 @@ class UserController extends Controller
     public function all()
     {
 
-        $loginList = [];
+        $loginHistory = [];
 
         $keys = Redis::keys('*');
         foreach ($keys as $key) {
             $key = substr($key, strlen(config('app.name').'_database_'));
             if (strlen($key) == 32) {
-                $loginList[$key] = Redis::smembers($key);
+                $loginHistory[$key] = Redis::smembers($key);
             }
         }
 
         return [
-            'loginList' => $loginList,
-            'skey_list' => Redis::hgetall('skey_list'),
+            'loginHistory' => $loginHistory,
+            'clientKeyList' => Redis::hgetall('clientKeyList'),
         ];
     }
 }
